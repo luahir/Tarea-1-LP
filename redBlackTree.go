2a94f60f4d346a02e62313138c8207dee573cf20
L. Antonio Hidalgo Rodríguez <lhidalgorodriguez@gmail.com>
2014-08-29 11:16:39 -0600

    comp added

--- redBlackTree.go
+++ redBlackTree.go
@@ -26,8 +26,8 @@ type rbTreer interface {
 type Color bool
 
 // Función para desplegar el color como un string
-func (color Color) String() string {
-      switch color {
+func (pColor Color) String() string {
+      switch pColor {
       case true:
             return "Negro"
       default:
@@ -46,20 +46,57 @@ type Node struct {
       parent *Node
 }
 
-// Getters - como value puede ser más complicado no se indica su getter
+// Getters y setters - como value puede ser más complicado no se indica su getter
 
-func (node *Node) Color() Color {
-      return node.Color
+func (pNode *Node) Color() Color {
+      return pNode.color
 }
 
-func (node *Node) Parent() *Node {
-      return node.parent
+func (pNode *Node) SetColor(pColor Color) {
+      pNode.color = pColor
+}
+
+func (pNode *Node) Parent() *Node {
+      return pNode.parent
 }
 
 // Función para desplegar el valor y el color del nodo mediante print
-func (node *Node) String() string {
+func (pNode *Node) String() string {
       // Se le da formato al valor y al color. %v despliega la interfaz
       // de value como {sam {12345 67890}} (para nombre y números de teléfono
       // por ejemplo.
-      return fmt.Sprint("%v : %s)", node.value, n.Color())
+      return fmt.Sprint("%v : %s)", pNode.value, pNode.Color())
 }
+
+// Es necesario que se defina un método de comparación entre los contenidos del árbol rojinegro,
+// por lo que se define comparación entre enteros e hileras.
+// El comparador genérico permite que el árbol reciba el comparador como un tipo, para que pueda
+// comparar los valores dentro de los nodos.
+type Comp func (o1, o2 interface{}) int
+
+func IntComp(o1, o2 interface{}) int {
+      int1, int2 := o1.(int), o2.(int)
+
+      switch {
+      case int1 > int2:
+            return 1
+      case int1 < int2:
+            return -1
+      default:
+            return 0
+      }
+}
+
+func StringComp(o1, o2 string) int {
+      st1, st2 := o1.(string), o2.(string)
+
+      switch {
+      case st1 > st2:
+            return 1
+      case st1 < st2:
+            return -1
+      default:
+            return 0
+      }
+}
+
