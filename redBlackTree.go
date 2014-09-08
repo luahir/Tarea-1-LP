5e6a0b2fd25a67cc8c80769231dc2b320130da91
L. Antonio Hidalgo Rodríguez <lhidalgorodriguez@gmail.com>
2014-09-07 21:42:22 -0600

    find added

--- redBlackTree.go
+++ redBlackTree.go
@@ -7,6 +7,7 @@ package redBlackTree
 
 import (
       "fmt"
+      "strings"
 )
 
 const (
@@ -15,14 +16,14 @@ const (
 
 // Interfaz para el árbol rojinegro. Cualquier implementación del árbol
 // requiere estos métodos (según se indican en la tarea programada).
-type rbTreer interface {
-      NewTree()
+type RBTreer interface {
+      NewTree(Cmp)
       PrettyPrint()
       Clear()
-      Insert()
-      Delete()
-      Find()
-      InOrderPrint()
+      Insert(interface{})
+      Delete(interface{})
+      Find(interface{})
+      String()
 }
 
 // Se define el color como rojo o negro mediante una variable booleana.
@@ -80,12 +81,33 @@ func (pNode *Node) isRight() bool {
       return pNode == pNode.parent.right
 }
 
+func (pNode *Node) grandpa() *Node {
+      return pNode.parent.parent
+}
+
+func (pNode *Node) uncle() *Node {
+      if pNode.parent.isRight() {
+            return pNode.grandpa().left
+      }
+      return pNode.grandpa().right
+}
+
 // Función para desplegar el valor y el color del nodo mediante print.
 func (pNode *Node) String() string {
       // Se le da formato al valor y al color. %v despliega la interfaz
       // de value como {sam {12345 67890}} (para nombre y números de teléfono
       // por ejemplo.
       return fmt.Sprintf("(%v : %s)", pNode.value, pNode.Color())
+}
+
+// Función para borrar todos los campos de un nodo, para que pueda eliminarse del 
+// árbol al llamar tree.Clear()
+func(pNode *Node) clear() {
+      pNode.parent = nil
+      pNode.right = nil
+      pNode.left = nil
+      pNode.color = false
+      pNode.value = nil
 }
 
 // Es necesario que se defina un método de comparación entre los contenidos del árbol rojinegro,
@@ -136,7 +158,7 @@ func (tree *RBTree) Root() *Node {
       return tree.root
 }
 // Se define un nuevo árbol con un comparador y raíz nula.
-func NewRBTree(pCmp Cmp) *RBTree {
+func NewTree(pCmp Cmp) *RBTree {
       tree := &RBTree{root: nil, cmp: pCmp, count: 0}
       return tree
 }
@@ -144,7 +166,7 @@ func NewRBTree(pCmp Cmp) *RBTree {
 // Método de inserción en el árbol, que introduce el nodo con valor pValue.
 // Este método solamente inserta el valor como en un árbol de búsqueda binario
 // y no se usa directamente.
-func (tree *RBTree) InsertValue(pValue interface{}) *Node {
+func (tree *RBTree) insertValue(pValue interface{}) *Node {
       // Si la raíz no existe, se inserta una nueva y se aumenta el contador
       // de nodos.
       if tree.root == nil {
@@ -195,7 +217,7 @@ func (tree *RBTree) InsertValue(pValue interface{}) *Node {
 // este método utiliza insertValue y devuelve falso si el valor ya se encuentra
 // en el árbol. Si no está en el árbol se inserta y devuelve true.
 func (tree *RBTree) Insert(pValue interface{}) bool {
-      node := tree.InsertValue(pValue)
+      node := tree.insertValue(pValue)
 
       // Si el método devuelve nil, significa que no se insertó nada (el valor ya
       // existe en el árbol).
@@ -219,20 +241,11 @@ func (tree *RBTree) Insert(pValue interface{}) bool {
                   return true
             // Caso 3: tanto padre como tío son rojos, ambos deben repintarse.
             // negro y el abuelo se vuelve rojo.
-            case node.parent.parent.left != nil && node.parent.parent.left.color == ROJO:
-                  grandpa := node.parent.parent
-                  uncle := grandpa.left
+            case node.uncle() != nil && node.uncle().color == ROJO:
                   node.parent.color = NEGRO
-                  uncle.color = NEGRO
-                  grandpa.color = ROJO
-                  node = grandpa
-            case node.parent.parent.right != nil && node.parent.parent.right.color == ROJO:
-                  grandpa := node.parent.parent
-                  uncle := grandpa.right
-                  node.parent.color = NEGRO
-                  uncle.color = NEGRO
-                  grandpa.color = ROJO
-                  node = grandpa
+                  node.uncle().color = NEGRO
+                  node.grandpa().color = ROJO
+                  node = node.grandpa()
             // Caso 4: padre rojo, tío negro.
             case node.isRight() && node.parent.isLeft():
                   tree.rotLeft(node.parent)
@@ -320,5 +333,102 @@ func (tree *RBTree) rotLeft(P *Node) {
       P.parent = Q
 }
 
+
+// El iterador recorre todo el árbol y devuelve todos los nodos, en el orden requerido.
+type Iterator interface {
+      Iterate(*Node)
+}
+
+// El iterador in-order es una estructura que tiene un canal de nodos
+type InorderIterator struct {
+}
+
+// Mediante un canal se recorre el árbol en in-order.
+func (iter *InorderIterator) Iterate(node *Node) <-chan *Node {
+      // Canal de tipo *Node.
+      ch := make(chan *Node)
+
+      // Función de visita, que se necesita solamente para
+      // el iterador, en in-order.
+      var visit func(*Node)
+      visit = func(visitNode *Node) {
+            if visitNode.left != nil {
+                  visit(visitNode.left)
+            }
+
+            ch <- visitNode
+
+            if visitNode.right != nil {
+                  visit(visitNode.right)
+            }
+      }
+
+      // Se llama a una goroutine que se encarga de visitar el resto de los nodos.
+      go func() {
+            if node != nil {
+                  visit(node)
+            }
+            close(ch)
+      }()
+
+      return ch
+}
+
+// Determina si la llave pKey se encuentra entre los valores de los nodos del árbol, 
+// si lo encuentra devuelve true y si no, false, además del nodo que contiene el valor.
+// Find utiliza el iterador para obtener el valor solicitado.
+func (tree *RBTree) Find(pKey interface{}) (bool, *Node) {
+      // Recorre el árbol, obtiene los valores en un channel
+      // y compara el valor de pKey con el de cada nodo
+      iter := &InorderIterator{}
+      for node := range iter.Iterate(tree.root) {
+            if pKey == node.value {
+                  return true, node
+            }
+      }
+      return false, nil
+}
+
+// FindKey determina si el valor de pKey es parte de los valores en el árbol. Es un
+// wrapper que devuelve solamente el primer argumento de Find.
+func (tree *RBTree) FindKey(pKey interface{}) bool {
+      found,_ := tree.Find(pKey)
+      return found
+}
+
+// Despliega los elementos del árbol en in-order, para mostrarlos mediante fmt.Print().
+// La hilera resultante se obtiene de recorrer el árbol mediante el iterador.
+func (tree *RBTree) String() string {
+      iter := &InorderIterator{}
+      s := "{"
+      for node := range iter.Iterate(tree.root) {
+            s += fmt.Sprintf("%v ", node)
+      }
+      s = strings.TrimSpace(s)
+      s += "}"
+      return s
+}
+
+// Clear borra completamente el árbol mediante deleteAll. 
+func (tree *RBTree) Clear() {
+      deleteAll(tree.root)
+      tree.root = nil
+}
+
+// deleteAll elimina los nodos recursivamente, mediante un recorrido en postorder
+func deleteAll(node *Node) {
+      if node != nil {
+            deleteAll(node.left)
+            deleteAll(node.right)
+            node.clear()
+            node = nil
+      }
+}
+
 // Delete elimina el nodo que coincide con el valor dado. No hace nada
 // si la llave no existe
+func (tree *RBTree) Delete(pKey interface{}) {
+      _, nodeToDel := tree.Find(pKey)
+      nodeCopy := nodeToDel
+      copyColor := nodeCopy.color
+}
